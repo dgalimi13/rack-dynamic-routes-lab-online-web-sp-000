02ee4faea261cf79579e897b1286b24a9a424c44
danny galimi <dannygalimi13@aim.com>
2021-02-28 04:27:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,9 @@ class Application
     
     
  
-  if req.path.match("/items/")
+  if 
+    
+    req.path.match("/items/")
     item_name = req.path.split("/items/").last
     item = @@items.find{|item| item.name == item_name}
     if item  
